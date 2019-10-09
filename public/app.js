bb96f618dd354134e28fc41cc6f667352940d593
Jules Comte <comtejules@gmail.com>
2019-10-09 15:03:11 -0400

    Deploying test to mobile

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 function adjustHeights(){
-  if ($(window).width()<=800)
+  if ($(window).width()>1000)
   {
-    var x = $(window).height();
-    $(".image").css("height",2/3*$(window).width());
-    $(".name").css("height",2/3*$(window).width());
-  } else {
     var x = $(window).height();
     $(".image").css("height",x);
     $(".name").css("height",x);
+  }
+  else {
+    $(".image").css("height","auto");
+    $(".name").css("height","auto");
   }
 }
 
