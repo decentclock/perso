7ae518d24d06c618033317c8e418168eb3d3b619
Jules Comte <comtejules@gmail.com>
2019-10-09 13:13:36 -0400

    Handle mobile resizing and positioning

---
+++ public/app.js
@@ -0,0 +1,18 @@
+function adjustHeights(){
+  if ($(window).width()<=770)
+  {
+    var x = $(window).height();
+    $(".image").css("height","350px");
+    $(".name").css("height",4*x/3);
+  } else {
+    var x = $(window).height();
+    $(".image").css("height",x);
+    $(".name").css("height",x);
+  }
+}
+
+adjustHeights();
+
+$(window).resize(function(){
+  adjustHeights();
+});
